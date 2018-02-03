1b451ae9c4fa41d58eae5824ddcd2424428584d9
glauberrleite <contact@glauberrleite.com>
2018-02-03 17:18:15 -0300

    Initial commit
    Smith's and Sundaresan's methods implemented

---
+++ main.py
@@ -0,0 +1,95 @@
+import numpy
+import matplotlib.pyplot as pyplot
+
+def findTimes(data, y1, y2):
+    t1 = data[0, 0]
+    t2 = data[-1, 0]
+
+    # Finding t1 and t2 paying attention to step size
+    for i in range(len(data)):
+        if i == len(data) - 1:
+            # There isn't a next value
+            break
+        
+        if data[i, 1] <= y1 and data[i+1, 1] > y1:
+            t1 = data[i, 0]
+        elif data[i, 1] <= y2 and data[i+1, 1] > y2:
+            t2 = data[i, 0]
+            break
+
+    return [t1, t2]
+
+
+print "System identifier"
+
+print "Loading data"
+data = numpy.loadtxt("values.data", skiprows=1)
+print "Data loaded"
+
+print "Choose an Identification Method:"
+print "1 - Ziegler-Nichols"
+print "2 - Hagglund"
+print "3 - Smith"
+print "4 - Sundaresan-Krishnaswamy"
+opt = input("Method: ")
+
+y_r = data[-1, 1]
+
+if opt >= 1 and opt <= 4:
+    # First Order Identification Method
+    y1 = None
+    y2 = None
+    K_p = None
+    tau = None
+    delay = None
+
+    if opt == 1:
+        # Ziegler-Nichols
+        print "test"
+    elif opt == 2:
+        # Hagglund
+        print "test"
+    elif opt == 3:
+        # Smith
+    
+        y1 = 0.283 * y_r
+        y2 = 0.632 * y_r
+    
+        [t1, t2] = findTimes(data, y1, y2)
+
+        tau = 1.5 * (t2 - t1)
+        delay = 1.5 * t1 - 0.5 * t2
+
+    elif opt == 4:
+        # Sundaresan-Krishnaswamy
+
+        y1 = 0.353 * y_r
+        y2 = 0.853 * y_r
+
+        [t1, t2] = findTimes(data, y1, y2)
+
+        tau = 0.67 * (t2 - t1)
+        delay = 1.3 * t1 - 0.29 * t2
+
+    K_p = y2 - y1
+
+    print "The Transfer Function is: "
+    print str(K_p) + " * e^(-" + str(delay) + " * s)"
+    print "-------------------"
+    print str(tau) + " * s + 1)"
+
+    tf = r"$Kp * \frac{1}{Ts + 1} * e^{-delays}$"
+
+    estimative = (1 - numpy.exp(-(data[:,0] - delay)/tau)) * K_p
+
+    pyplot.plot(data[:, 0], data[:, 1], 'b', estimative, 'r')
+    pyplot.xlabel("Time (seconds)")
+    pyplot.title("Comparison")
+    pyplot.text(60, .025, tf)
+    pyplot.show()
+
+elif opt <= 5:
+    # Second Order Identification Method
+    print "test"
+else:
+    print "Invalid option"
