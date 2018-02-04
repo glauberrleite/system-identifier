c9a14a6d14f0b571a5d9acc995d7dcf5530757a1
glauberrleite <contact@glauberrleite.com>
2018-02-04 11:23:06 -0300

    Added 2 methods
    Implemented Ziegler-Nichols and Haggland

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy
 import matplotlib.pyplot as pyplot
 
-def findTimes(data, y1, y2):
+def findTimesOnData(data, y1, y2):
+    "Given a numpy array of time and respective output, locates the times where y1 and y2 are reached"
     t1 = data[0, 0]
     t2 = data[-1, 0]
 
@@ -16,6 +17,32 @@ def findTimes(data, y1, y2):
         elif data[i, 1] <= y2 and data[i+1, 1] > y2:
             t2 = data[i, 0]
             break
+
+    return [t1, t2]
+
+def findInflectionPoint(data):
+    "Given a numpy array of time and respective output, locates the inflection point and the derivative on it"
+    m = 0
+    x0 = data[0, 0]
+    y0 = data[0, 1]
+
+    for i in range(len(data)):
+        delta = abs((data[i, 1] - data[i + 1, 1]) / (data[i, 0] - data[i + 1, 0]))
+        if delta < m:
+            x0 = data[i, 0]
+            y0 = data[i, 1]
+            break
+        m = delta
+    
+    return [x0, y0, m]
+
+def findTimesOnTangentLine(x0, y0, m, y1, y2):
+    # Tangent line equation: (y - y0) = m * (x - x0)
+    # For t1: (y1 - y0) = m * (t1 - x0)
+    t1 = x0 + (y1 - y0) / m
+
+    # For t2: (y2 - y0) = m * (t2 - x0)
+    t2 = x0 + (y2 - y0) / m
 
     return [t1, t2]
 
@@ -45,17 +72,36 @@ if opt >= 1 and opt <= 4:
 
     if opt == 1:
         # Ziegler-Nichols
-        print "test"
+        [x0, y0, m] = findInflectionPoint(data)
+        
+        y1 = 0
+        y2 = y_r
+
+        [t1, t2] = findTimesOnTangentLine(x0, y0, m, y1, y2)
+
+        delay = t1
+        tau = t2 - t1
+        
     elif opt == 2:
         # Hagglund
-        print "test"
+        [x0, y0, m] = findInflectionPoint(data)
+
+        y1 = 0
+        y2 = 0.632 * y_r
+
+        [t1, t2] = findTimesOnTangentLine(x0, y0, m, y1, y2)
+
+        delay = t1
+        tau = t2 - t1
+
+        
     elif opt == 3:
         # Smith
     
         y1 = 0.283 * y_r
         y2 = 0.632 * y_r
     
-        [t1, t2] = findTimes(data, y1, y2)
+        [t1, t2] = findTimesOnData(data, y1, y2)
 
         tau = 1.5 * (t2 - t1)
         delay = 1.5 * t1 - 0.5 * t2
@@ -66,7 +112,7 @@ if opt >= 1 and opt <= 4:
         y1 = 0.353 * y_r
         y2 = 0.853 * y_r
 
-        [t1, t2] = findTimes(data, y1, y2)
+        [t1, t2] = findTimesOnData(data, y1, y2)
 
         tau = 0.67 * (t2 - t1)
         delay = 1.3 * t1 - 0.29 * t2
